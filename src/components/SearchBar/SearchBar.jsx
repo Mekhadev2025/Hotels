f14281a7295433d583c18fb4030e89496f507348
Mekha <mekhalnair@gmail.com>
2023-10-06 21:05:18 +0530

    adding searchbar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -11,36 +11,14 @@ const SearchBar = (props) => {
 
   return (
     <div className="srchContainer">
-      <div className="searchBar">
-        <input
-          className="searchText"
-          type="text"
-          placeholder="Search"
-          value={value}
-          onChange={(event) => {
-            const updatedValue = event.target.value; // Get the updated value
-            setValue(updatedValue); // Update the state with the new value
-            props.handleValue(updatedValue); // Pass the updated value to the prop function
-          }}
-        ></input>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          fill="currentColor"
-          className="bi bi-search"
-          viewBox="0 0 16 16"
-          onClick={handleClick}
-        >
-          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-        </svg>
+      <div className="citygroup">
+        <button className="btn newYork"></button>
+        <button className="btn mumbai"></button>
+        <button className="btn paris"></button>
+        <button className="btn london"></button>
       </div>
-      <select className="dd">
-        <option>Relevance</option>
-      </select>
-      <select className="dd">
-        <option>All Brands</option>
-      </select>
+     <div className="viewAll">View All  </div>
+     
     </div>
   );
 };
