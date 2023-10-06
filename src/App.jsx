c2ae9ce5b35ebaabe71173439b0a53d0ca3984ff
Mekha <mekhalnair@gmail.com>
2023-10-06 22:41:03 +0530

    adding changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SearchBar from "./components/SearchBar/SearchBar";
-import Home from "./components/Home/Home"
-import React,{useState} from "react"
+import Home from "./components/Home/Home";
+import React, { useState } from "react";
 function App() {
-  const [value,setValue]=useState(null)
-  const handleValue=(values)=>{
-      setValue(values)
-      console.log("Hurray",value)
-  }
+  const [value, setValue] = useState(null);
+  const handleValue = (values) => {
+    setValue(values);
+    console.log("Hurray", value);
+  };
   return (
     <>
       <BrowserRouter>
-       <SearchBar handleValue={handleValue}/>
+        <SearchBar handleValue={handleValue} />
         <Routes>
-         
-          <Route path="/" element={<Home values={value}/>} ></Route>
+          <Route path="/" element={<Home values={value} />}></Route>
         </Routes>
       </BrowserRouter>
     </>
